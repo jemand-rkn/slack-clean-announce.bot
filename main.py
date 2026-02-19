241bb4e6eb8fa60363ed75945bb5bdeeb2458402
Kentaro Nomura <k.nomura@rlg.sys.es.osaka-u.ac.jp>
2026-02-19 17:57:07 +0900

    update spread sheet url

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ name_id_dict = json.loads(name_id_json) if name_id_json else None
 
 #print(cell_value1, cell_value2)
 #records = sheet.get_all_records()
-message = f"【{cleen_date}掃除連絡】\nD455:  <@{name_id_dict[D455_1]}>  <@{name_id_dict[D455_2]}>\n実験室:  <@{name_id_dict[experiment_1]}>  <@{name_id_dict[experiment_2]}> \nF322:  <@{name_id_dict[F322_1]}>  <@{name_id_dict[F322_2]}>\nH402(New): <@{name_id_dict[H402_1]}> <@{name_id_dict[H402_2]}>\n\n・掃除・ゴミ出し一回ごとに日付を記入\nhttps://docs.google.com/spreadsheets/d/1y--2Lzvpgk8O7MlgBUZiLJq0LKIoglfNWaBG_bHfynk/edit?gid=343667719#gid=343667719\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能"
+message = f"【{cleen_date}掃除連絡】\nD455:  <@{name_id_dict[D455_1]}>  <@{name_id_dict[D455_2]}>\n実験室:  <@{name_id_dict[experiment_1]}>  <@{name_id_dict[experiment_2]}> \nF322:  <@{name_id_dict[F322_1]}>  <@{name_id_dict[F322_2]}>\nH402(New): <@{name_id_dict[H402_1]}> <@{name_id_dict[H402_2]}>\n\n・掃除・ゴミ出し一回ごとに日付を記入\nhttps://docs.google.com/spreadsheets/d/1y--2Lzvpgk8O7MlgBUZiLJq0LKIoglfNWaBG_bHfynk/edit?gid=652942208#gid=652942208\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能"
 # # Slack メッセージ整形
 # message = "*今週の報告*\n"
 # for row in records[-5:]:  # 最新の5件

3fb7ccbd12ec82d3a0092e6fae6db2aefb8e8bb7
Kentaro Nomura <k.nomura@rlg.sys.es.osaka-u.ac.jp>
2026-04-08 20:06:03 +0900

    edit message

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ sheet_url = os.environ.get("SHEET_URL")
 
 #print(cell_value1, cell_value2)
 #records = sheet.get_all_records()
-message = f"【{cleen_date}掃除連絡】\nD455:  <@{name_id_dict[D455_1]}>  <@{name_id_dict[D455_2]}>\n実験室:  <@{name_id_dict[experiment_1]}>  <@{name_id_dict[experiment_2]}> \nF322:  <@{name_id_dict[F322_1]}>  <@{name_id_dict[F322_2]}>\nH402(New): <@{name_id_dict[H402_1]}> <@{name_id_dict[H402_2]}>\nJ213:  <@{name_id_dict[J213_1]}>  <@{name_id_dict[J213_2]}>\n\n・掃除・ゴミ出し一回ごとに日付を記入\n{sheet_url}\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能"
+message = f"【{cleen_date}掃除連絡】\nD455:  <@{name_id_dict[D455_1]}>  <@{name_id_dict[D455_2]}>\n実験室:  <@{name_id_dict[experiment_1]}>  <@{name_id_dict[experiment_2]}> \nF322:  <@{name_id_dict[F322_1]}>  <@{name_id_dict[F322_2]}>\nH402: <@{name_id_dict[H402_1]}> <@{name_id_dict[H402_2]}>\nJ213:  <@{name_id_dict[J213_1]}>  <@{name_id_dict[J213_2]}>\n\n・掃除・ゴミ出し一回ごとに日付を記入\n{sheet_url}\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能"
 # # Slack メッセージ整形
 # message = "*今週の報告*\n"
 # for row in records[-5:]:  # 最新の5件
